b1ed7e42b370292b3ca6c604e1c1cdff2f98a75b
Tamir Aminov <tamir.aminov@gmail.com>
2015-01-04 17:48:42 +0200

    feat(zsh): Add install script for zsh

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 clear
-echo "Good morning, world."
+echo "Installing zsh"
+
+curl -L http://install.ohmyz.sh | sh
+cd ~/.oh-my-zsh/custom/plugins
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+cd $CURRENT_DIRECTORY
+# forces terminal to use zsh
+echo "\nzsh && exit 0" >> ~/.bash_profile
+source ~/.bash_profile
